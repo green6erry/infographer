593db3890765aa69013e9164a8904f164159b72c
green6erry <green6erry@gmail.com>
2016-05-11 23:02:06 +0200

    testing
    still no purpose yet

--- js/app.js
+++ js/app.js
@@ -82,7 +82,11 @@ var Pie = {
     "enabled": true
   }
 }
+
+var apiType = '';
 var marvelchart = Object.create(Pie);
+
+var nextPageToken, prevPageToken, query, findex;
 marvelchart = {
   "theme": "dark",
 };
@@ -93,9 +97,11 @@ function viewCharts(topic){
   var chart = AmCharts.makeChart( "chartdiv", Pie);
 }
 
-function getRequest(searchTerm, pageToken) {
-  console.log('get');
 
+
+//4.
+
+function getterfallTube(searchTerm, pageToken){
   var params = {
     r: 'json',
     q: searchTerm,
@@ -103,55 +109,77 @@ function getRequest(searchTerm, pageToken) {
     order: 'viewCount',
     pageToken: pageToken,
     maxResults: 10,
-    key:'AIzaSyA5KnfmKw5qQc6iFwxuLlXw2lgd5ydWb8M',
+    key: 'AIzaSyA5KnfmKw5qQc6iFwxuLlXw2lgd5ydWb8M'
+
   };
-
- 
+  url = 'https://www.googleapis.com/youtube/v3/search';
   $.getJSON(url, params, function(data){
-
+    console.log(data.items);
     showResults(data.items);
+
     nextPageToken = data.nextPageToken;
     prevPageToken = data.prevPageToken;
     query = searchTerm;
     });
 }
 
-// 2.
-//Curren
-function selectGet(apiSelected){
+//3.
 
-  if (apiSelected == 'nasa'){
-  var nasa = {
-    url: 'https://api.nasa.gov/planetary/apod?api_key=',
-    key: 'pafCzaHE6vFx9Pu1alThTDHibWb57O0aTd95rwnp',
-  };
+function addResults(results, api){
+  console.log('show');
+  var html=' ';
+  $.each(results, function(index, value){
+    var result = results[index];
+    console.log(result);
+    
+    html += '<p>'+result.firstName+'</p><h2>'+result.fullName+'</h2>';
+    
+
+    $('#results-index').append('<div id="results-'+api+'">'+html+'</div>');
+
+  });
+
+  
 }
 
-  else if (apiSelected == 'marvel'){
-  var url = 'http://gateway.marvel.com:80/v1/public/characters';
-  var privateKey = 'ef9561628f5de17da7dc979e830fe93c2a1962a0';
-  var params = {
-    apikey: 'ad6db08464ed1fdeb0350ccb3488b073',
-    ts: Date.now(),
-    }
-    params.hash = md5(params.ts+privateKey+params.apikey);
-  }
+// 2.
 
+function getterfallMarvel(searchTerm){
+
+  apiType = 'marvel';
+  var url = 'http://gateway.marvel.com:80/v1/public/creators';
+  var privateKey = 'ef9561628f5de17da7dc979e830fe93c2a1962a0';
+  var input = searchTerm;
+  var params = {
+
+    firstName: input,
+    apikey: 'ad6db08464ed1fdeb0350ccb3488b073',
+    ts: Date.now()
+    };
+  params.hash = md5(params.ts+privateKey+params.apikey);
 
   $.getJSON(url, params, function(data){
-    // viewCharts(data.items);
-    event.preventDefault();
-    console.log(data);
+    if(data.data.total > 0) {
+      addResults(data.data.results, apiType);
+      var logo = '<a href="www.marvel.com"><img src="images/marvel.png" alt="Marvel logo" /></a>';
+
+    }
+
+    getterfallTube(searchTerm);
   });
+
 }
 
+// 0. 
+$('input[type="text"]').focus();
 
 // 1. Start here
-$('form').submit(function(choice){
-  var topic = $('input:checked').val();
-  // console.log(topic);
-  selectGet(topic);
-  $('input:checked').prop('checked', false);
+$('form').submit(function(event){
+  event.preventDefault();
+  var searchTerm = $('input[type="text"]').val();
+  console.log(searchTerm);
+  getterfallMarvel(searchTerm);
+  $('input[type="text"]').val('');
 });
 
 
