ba9d672404c0f5590a2db8424edd4aa2cebbb261
green6erry <green6erry@gmail.com>
2016-06-15 10:56:36 -0400

    NPM
    Trying to get NPM to work

---
+++ js/background.js
@@ -0,0 +1,7 @@
+var $ = require('jquery');
+
+var makeBackgroundRed = function() {
+    $('body').css('background-color', '#F44336');
+};
+
+module.exports = makeBackgroundRed;
---
+++ js/index.js
@@ -0,0 +1,14 @@
+
+require('async');
+require('packery'); 
+
+
+var $ = require('jquery');
+var makeBackgroundRed = require('./background');
+
+
+$(document).ready(function() {
+	makeBackgroundRed();
+});
+
+require('./app');
